1487ce0f1d970193ceaeb3321c145f682b422841
cdkim1226 <cdkim1226@gmail.com>
2020-10-12 20:17:33 +0900

    Apply AppBar & Font for Design

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.use('/image', express.static('./upload'));
 
 app.post('/api/customers', upload.single('image'), (req, res) => {
   let sql = 'INSERT INTO customer VALUES(NULL,?,?,?,?,?,now(),0)';
-  let image = '/image/' + req.file.filename;
+  let image = '/image' + req.file.filename;
   let name = req.body.name;
   let birthday = req.body.birthday;
   let gender = req.body.gender;
